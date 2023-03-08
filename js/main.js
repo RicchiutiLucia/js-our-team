cf2fb2ea99a273eb2a622537f5b7a018a0048904
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-03-08 14:12:31 +0100

    Milestone 0

--- js/main.js
+++ js/main.js
@@ -10,3 +10,36 @@ BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede */
+
+const ourTeam = [
+    {
+        'nome':'Wayne Barnett',
+        'ruolo':'Founder & CEO',
+        'immagine':'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome':'Angela Caroll',
+        'ruolo':'Chief Editor',
+        'immagine':'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nome':'Walter Gordon',
+        'ruolo':'Office Manager',
+        'immagine':'walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome':'Angela Lopez',
+        'ruolo':'Social Media Manager',
+        'immagine':'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome':'Scott Estrada',
+        'ruolo':'Developer',
+        'immagine':'scott-estrada-developer.jpg'
+    },
+    {
+        'nome':'Barbara Ramos',
+        'ruolo':'Graphic Designer',
+        'immagine':'barbara-ramos-graphic-designer.jpg'
+    }
+];
